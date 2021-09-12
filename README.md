d5d060feca722ef64ed17a833a8c92a9fe650f5b
Jatinder Kumar Chaurasia <jatinderkumarchaurasia@gmail.com>
2021-09-12 15:27:10 +0530

    added openapi-docs

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-podman-compose up --build -d
-podman-compose push
-podman rmi $(podman images -q -f dangling=true)
-podman logs -f --details  containerID
-podman system prune -f --volumes
+
+###### Product Composite Link
+    http://localhost:8080/product-composite/1
+
+##### swagger ui link
+    http://localhost:8080/openapi/swagger-ui.html   
+
+##### podman commands to build
+    podman-compose up --build -d
+    podman-compose push
+    podman rmi $(podman images -q -f dangling=true)
+    podman logs -f --details  containerID
+    podman system prune -f --volumes
+    podman container logs container_name
+
+
+    
--- microservices/flowkind-product-compose-service/src/main/java/app/flowkind/microservices/compose/product/FlowkindProductComposeServiceApplication.java
+++ microservices/flowkind-product-compose-service/src/main/java/app/flowkind/microservices/compose/product/FlowkindProductComposeServiceApplication.java
@@ -1,5 +1,11 @@
 package app.flowkind.microservices.compose.product;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +15,28 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @ComponentScan("app.flowkind")
 public class FlowkindProductComposeServiceApplication {
+
+    @Value("${api.common.version}") String apiVersion;
+    @Value("${api.common.title}")           String apiTitle;
+    @Value("${api.common.description}")     String apiDescription;
+    @Value("${api.common.termsOfService}")  String apiTermsOfService;
+    @Value("${api.common.license}")         String apiLicense;
+    @Value("${api.common.licenseURL}")      String apiLicenseUrl;
+    @Value("${api.common.externalDocDesc}") String apiExternalDocDesc;
+    @Value("${api.common.externalDocUrl}")  String apiExternalDocUrl;
+    @Value("${api.common.contact.name}")    String apiContactName;
+    @Value("${api.common.contact.url}")     String apiContactUrl;
+    @Value("${api.common.contact.email}")   String apiContactEmail;
+
+    /**
+     * Will exposed on $HOST:$PORT/swagger-ui.html
+     *
+     * @return the common OpenAPI documentation
+     */
+    @Bean
+    public OpenAPI getOpenAPIDocumentation() {
+        return new OpenAPI().info(new Info().title(apiTitle).description(apiDescription).version(apiVersion).contact(new Contact().name(apiContactName).url(apiContactUrl).email(apiContactEmail)).termsOfService(apiTermsOfService).license(new License().name(apiLicense).url(apiLicenseUrl))).externalDocs(new ExternalDocumentation().url(apiExternalDocUrl).description(apiExternalDocDesc));
+    }
 
     @Bean
     RestTemplate restTemplate() {
